5412a609b0b1338719a60d29d38df10e8a2a010c
刘一 <liuyi111111@live.com>
2018-05-16 17:45:08 +0800

    添加了IsSubclassOf等帮助方法

---
+++ Analyzer1/Analyzer1/Helper/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+// ReSharper disable once CheckNamespace
+namespace DiagnosticTools.Utilities
+{
+    public static class Extensions
+    {
+        public static bool IsSubclassOf(this ITypeSymbol symbol, Type type)
+        {
+            var @base = symbol.BaseType;
+
+            while (@base != null)
+            {
+                if (@base.Name == type.Name) { return true; }
+                @base = @base.BaseType;
+            }
+
+            return false;
+        }
+
+        public static bool ImplementsInterface(this ITypeSymbol symbol, Type type)
+        {
+            return symbol.Interfaces.Any(x => x.Name == type.Name);
+        }
+
+        public static bool IsCollection(this ITypeSymbol symbol)
+        {
+            return symbol.Interfaces.Any(x => x.Name == typeof(IEnumerable).Name
+                                              || x.Name == typeof(IEnumerable<>).Name);
+        }
+    }
+}
---
+++ Analyzer1/Analyzer1/Helper/RoslynUtils.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DiagnosticTools.Utilities
+{
+    public static class RoslynUtils
+    {
+        public static ExpressionSyntax GetInstantiationExpressionFromType(string typeName, TypeArgumentListSyntax typeArgumentsSyntax)
+        {
+            // 0: Name of type
+            // 1: (Optional) generic arguments
+            // 2: (Optional) arguments
+            var result = " new {0}{1}({2})";
+            var typeArguments = GetParsedTypeArguments(typeArgumentsSyntax);
+            var arguments = ""; // Not implemented until I find out a place where to use them
+
+            return SyntaxFactory.ParseExpression(string.Format(result, typeName, typeArguments, arguments));
+        }
+
+        private static string GetParsedTypeArguments(TypeArgumentListSyntax arguments)
+        {
+            if (arguments.Arguments.Count == 0) { return ""; }
+
+            var sb = new StringBuilder("<");
+            for (var i = 0; i < arguments.Arguments.Count; i++)
+            {
+                sb.Append(arguments.Arguments[i].ToString());
+                if (i != arguments.Arguments.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append(">");
+            return sb.ToString();
+        }
+    }
+}
